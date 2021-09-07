bb70c5761bffbc9ccba4771fc40a97976a58799f
yuri <sanchez.yurifer@gmail.com>
2021-09-07 11:22:35 +0200

    test: fix test

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class FizzBuzzTest {
 
     @ParameterizedTest
-    @CsvSource({"3,Fizz", "5,Buzz", "7,fizzbuzz.FizzBuzz"})
+    @CsvSource({"3, 3 Fizz", "5, 5 Buzz", "7, 7 FizzBuzz"})
     void playFizzBuzz(String num, String expected ){
 
         String result  = FizzBuzz.playFizzBuzz(Integer.parseInt(num));
